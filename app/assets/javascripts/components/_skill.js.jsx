144ca66bf28b7cebce082d44381d62a668100fbd
David Maurer <davemaurermail@gmail.com>
2015-12-20 17:10:39 -0700

    buttons for upvote and downvote with a little style

--- app/assets/javascripts/components/_skill.js.jsx
+++ app/assets/javascripts/components/_skill.js.jsx
@@ -27,7 +27,17 @@ var Skill = React.createClass({
     return (
       <div>
         {name}
-        <p><strong>Level:</strong> {this.props.skill.level}</p>
+        <div className='skill-level'>
+          <button type="button" className="btn btn-default btn-sm">
+            <span className="glyphicon glyphicon-triangle-bottom"></span>
+          </button>
+
+          <p><strong>Level:</strong> {this.props.skill.level}</p>
+
+          <button type="button" className="btn btn-default btn-sm">
+            <span className="glyphicon glyphicon-triangle-top"></span>
+          </button>
+        </div>
 
         {details}
         <button onClick={this.props.handleDelete}>Delete</button>
